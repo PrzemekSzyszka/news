601f7da4d34219d32d0f6ef06e04b5208623ba2a
Przemysław Szyszka <przemeklo@o2.pl>
2014-12-22 18:38:54 +0100

    #1 - Create test for basic api

---
+++ test/news_test.rb
@@ -0,0 +1,17 @@
+require "rubygems"
+require "rack/test"
+require 'minitest/autorun'
+require_relative '../lib/news.rb'
+
+class NewsTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    News.new
+  end
+
+  def test_app_returns_an_response
+    get "/"
+    assert_equal "Hello News!", last_response.body
+  end
+end
