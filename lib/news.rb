7d8c04d300e4fd2fbb9102c1d214cf00df8059a6
Przemysław Szyszka <przemeklo@o2.pl>
2014-12-23 10:37:06 +0100

    #4 - Prepare json responses using sinatra

--- lib/news.rb
+++ lib/news.rb
@@ -1,11 +1,33 @@
 require 'sinatra'
+require 'json'
 
 class News < Sinatra::Base
-  def call(env)
-    [200, {"Content-Type" => "text/html"}, "Hello News!"]
+  get '/' do
+    'Hello News!'
   end
 
-  get '/' do
-    "Hello World"
+  get '/stories' do
+    content_type :json
+    [
+      {
+        id: 1,
+        title: 'title1',
+        url: 'http://www.lipton1.com'
+      },
+      {
+        id: 2,
+        title: 'title2',
+        url: 'http://www.lipton2.com'
+      }
+    ].to_json
+  end
+
+  get '/stories/:id' do
+    content_type :json
+    {
+      id: params[:id],
+      title: 'title',
+      url: 'http://www.lipton.com'
+    }.to_json
   end
 end
