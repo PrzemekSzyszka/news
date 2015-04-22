91e154609e1dcd4cef704edeac4be05ec4ec3619
Przemysław Szyszka <przemeklo@o2.pl>
2015-04-22 09:51:33 +0200

    #1 - Create error when record is invalid

--- lib/api/base.rb
+++ lib/api/base.rb
@@ -14,6 +14,20 @@ module API
       content_type :json
       status 404
 
+
+      error_message
+    end
+
+    error ActiveRecord::RecordInvalid do
+      content_type :json
+      status 400
+
+      error_message
+    end
+
+    private
+
+    def error_message
       e = env['sinatra.error']
       { error: e.message }.to_json
     end
