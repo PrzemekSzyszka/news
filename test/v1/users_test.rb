0581b17b9011cd26ce73479ed8dfe01e5ec20585
Przemysław Szyszka <przemeklo@o2.pl>
2015-05-20 19:49:46 +0200

    #3 - Set version header in all requests

--- test/v1/users_test.rb
+++ test/v1/users_test.rb
@@ -12,12 +12,13 @@ class UsersTest < ActiveSupport::TestCase
   end
 
   def test_creates_a_user
-    post '/v1/users', { username: 'Alan', password: 'Ala1Ma2Kota' }.to_json, { "CONTENT_TYPE" => "application/json" }
+    post '/users', { username: 'Alan', password: 'Ala1Ma2Kota' }.to_json,
+                   { 'CONTENT_TYPE' => 'application/json', 'HTTP_ACCEPT' => 'version=1' }
     assert_equal 201, last_response.status
   end
 
   def test_fails_to_create_a_user_without_username
-    post '/v1/users', { password: 'Ala1Ma2Kota' }.to_json, { "CONTENT_TYPE" => "application/json" }
+    post '/users', { password: 'Ala1Ma2Kota' }.to_json, { 'CONTENT_TYPE' => 'application/json', 'HTTP_ACCEPT' => 'version=1' }
     assert_equal 422, last_response.status
 
     data = JSON.parse last_response.body
